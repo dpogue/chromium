4e61e5ebc8eeb0a4f4c684561dcc36375ca46ea3
Darryl Pogue <darryl@dpogue.ca>
2026-04-24 14:12:01 -0700

    [AW] Support lib WebChromeClientCompat plumbing

---
+++ android_webview/support_library/boundary_interfaces/src/org/chromium/support_lib_boundary/WebChromeClientBoundaryInterface.java
@@ -0,0 +1,12 @@
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.support_lib_boundary;
+
+import org.jspecify.annotations.NullMarked;
+
+/** Boundary interface for WebChromeClientCompat. */
+@NullMarked
+public interface WebChromeClientBoundaryInterface extends FeatureFlagHolderBoundaryInterface {
+}
